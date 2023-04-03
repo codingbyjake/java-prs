f1067a485b5afe3eb059736cfce179cb339c4eac
Jacob Witte <wittejg@gmail.com>
2023-04-03 15:52:37 -0400

    added Product Class, Repo and Controller

--- src/main/java/com/maxtrain/javaprs/Product/Product.java
+++ src/main/java/com/maxtrain/javaprs/Product/Product.java
@@ -1,5 +1,88 @@
 package com.maxtrain.javaprs.Product;
 
-public class Product {
+import com.maxtrain.javaprs.Vendor.Vendor;
+import jakarta.persistence.*;
 
+
+@Entity
+@Table(name="Products", uniqueConstraints=@UniqueConstraint(name="UDIX_Code", columnNames = {"partNbr"}))
+public class Product {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Column(length=30, nullable = false)
+	public String partNbr;
+	@Column(length=30, nullable = false)
+	public String name;
+	@Column(columnDefinition="decimal(11,2) NOT NULL")
+	public double price;
+	@Column(length=30, nullable = false)
+	public String unit;
+	@Column(length=255, nullable = true)
+	public String photoPath;
+	
+	// FK's and Virtual Props
+	@ManyToOne(optional=false)
+	@JoinColumn(name="vendorId", columnDefinition="int")
+	public Vendor vendor;
+
+	
+	// Getters and Setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPartNbr() {
+		return partNbr;
+	}
+
+	public void setPartNbr(String partNbr) {
+		this.partNbr = partNbr;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public String getPhotoPath() {
+		return photoPath;
+	}
+
+	public void setPhotoPath(String photoPath) {
+		this.photoPath = photoPath;
+	}
+
+	public Vendor getVendor() {
+		return vendor;
+	}
+
+	public void setVendor(Vendor vendor) {
+		this.vendor = vendor;
+	}
+
+	
 }
